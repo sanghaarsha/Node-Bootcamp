4a59221209fdfd67e8a67e84fca5943ae6c1cd7d
sanghaarsha <sanghaarsha@gmail.com>
2021-07-18 14:32:03 +0545

    dotenv setup

--- 06. Express/app.js
+++ 06. Express/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+// dotenv setup
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+
+console.log(TEST);
 
 app.get("/", (req, res) => {
   res.send("Hello from express app!");
