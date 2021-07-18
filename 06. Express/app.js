d8a30f83d3ab93b02b780d66958eb063dc22aa63
sanghaarsha <sanghaarsha@gmail.com>
2021-07-18 19:27:52 +0545

    creating middlewares

--- 06. Express/app.js
+++ 06. Express/app.js
@@ -3,7 +3,18 @@ const express = require("express");
 const app = express();
 
 // middlewares
-app.use(express.json());
+//('global' middlewares fires up for every request is made)
+app.use(express.json()); // express built-in middleware
+// custom middleware:
+app.use((req, res, next) => {
+  console.log("Hello, from the middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // dotenv setup
 require("dotenv").config();
@@ -19,33 +30,44 @@ const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
-    data: { tours },
+    data: {
+      tours,
+    },
   });
 };
 
 const getTourById = (req, res) => {
+  // accessing the time inserted into req object from custom middleware
+  console.log(req.requestTime);
+
   const reqId = req.params.id * 1;
   const tour = tours.find((el) => el.id === reqId);
   if (tour) {
-    res
-    .status(200)
-    .json({
+    res.status(200).json({
       status: "success",
+      // can also send this in a response
+      requestedAt: req.requestTime,
       data: {
         tour: tour,
       },
     });
   } else {
-    res
-    .status(404)
-    .json({ status: "failed", message: "Invalid ID" });
+    res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
   }
 };
 
 const postNewTour = (req, res) => {
   console.log(req.body);
   const newID = tours.length;
-  const newObj = Object.assign({ id: newID }, req.body);
+  const newObj = Object.assign(
+    {
+      id: newID,
+    },
+    req.body
+  );
   tours.push(newObj);
   fs.writeFile(
     __dirname + "/dev-data/data/tours-simple.json",
@@ -53,9 +75,7 @@ const postNewTour = (req, res) => {
     (err) => {
       if (err) console.log(err);
 
-      res
-      .status(201)
-      .json({
+      res.status(201).json({
         status: "success",
         data: {
           tour: newObj,
@@ -72,11 +92,16 @@ const patchTour = (req, res) => {
   console.log(req.body);
 
   if (tour) {
-    res
-      .status(200)
-      .json({ todo: "Update This!", receivedId: reqId, toUpdate: tour });
+    res.status(200).json({
+      todo: "Update This!",
+      receivedId: reqId,
+      toUpdate: tour,
+    });
   } else {
-    res.status(404).json({ status: "failed", message: "Invalid ID" });
+    res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
   }
 };
 
@@ -84,23 +109,28 @@ const deleteTour = (req, res) => {
   const reqId = req.params.id * 1;
   const tour = tours.find((el) => el.id === reqId);
   if (tour) {
-    res.status(204).json({ status: "success", data: null });
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
   } else {
-    res.status(404).json({ status: "failed", message: "Invalid ID" });
+    res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
   }
 };
 
 // Routes
-app
-.route("/api/v1/tours")
-.get(getAllTours)
-.post(postNewTour);
+app.route("/api/v1/tours").get(getAllTours).post(postNewTour);
 
 app
   .route("/api/v1/tours/:id")
   .get(getTourById)
   .patch(patchTour)
   .delete(deleteTour);
+
+app.use((abc, def) => def.status(404).send("resource not found!"));
 
 // Listen the app at PORT
 app.listen(PORT, () => {
