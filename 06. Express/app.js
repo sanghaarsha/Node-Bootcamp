96dde53ed5796bea31cc7a35d88d2de886fd263a
sanghaarsha <sanghaarsha@gmail.com>
2021-07-18 15:12:04 +0545

    simple api

--- 06. Express/app.js
+++ 06. Express/app.js
@@ -1,3 +1,7 @@
+// core modules
+const fs = require("fs");
+
+// npm modules
 const express = require("express");
 const app = express();
 
@@ -5,14 +9,18 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-console.log(TEST);
+//Blocking Codes : (because, before event loop : only happens once)
+const tours = JSON.parse(
+  fs.readFileSync(__dirname + "/dev-data/data/tours-simple.json")
+);
 
-app.get("/", (req, res) => {
-  res.send("Hello from express app!");
-});
-
-app.post("/", (req, res) => {
-  res.send("You can post to this route!");
+// Non Blocking Codes (runs every time req is received):
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: { tours },
+  });
 });
 
 app.listen(PORT, () => {
