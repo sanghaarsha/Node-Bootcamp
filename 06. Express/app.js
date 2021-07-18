8c9a6fd81822e620488dd16d0b5371729832a7d1
sanghaarsha <sanghaarsha@gmail.com>
2021-07-18 17:40:37 +0545

    API POST request

--- 06. Express/app.js
+++ 06. Express/app.js
@@ -5,6 +5,9 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
+// middlewares
+app.use(express.json()); // for parsing requests
+
 // dotenv setup
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +24,33 @@ app.get("/api/v1/tours", (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+// POST requests
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  const newID = tours.length;
+
+  // creating new object by merging to existing objects
+  const newObj = Object.assign({ id: newID }, req.body);
+  tours.push(newObj);
+
+  // as we do not want to lock the event loop, we will use async fs.writeFile
+  fs.writeFile(
+    __dirname + "/dev-data/data/tours-simple.json",
+    JSON.stringify(tours),
+    (err) => {
+      if (err) console.log(err);
+
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newObj,
+        },
+      });
+    }
+  );
+
 });
 
 app.listen(PORT, () => {
