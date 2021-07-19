9aa698f4ca52f261aa2eb7921962e6379fe911fe
sanghaarsha <sanghaarsha@gmail.com>
2021-07-19 11:10:15 +0545

    configuring environment variables

--- 06. Express/server.js
+++ 06. Express/server.js
@@ -1,8 +1,11 @@
-const app = require("./app");
-
 // Dotenv Setup
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+
+const app = require("./app");
+
+// env set by express -> app.get("env")
+// env set by node -> process.env
 
 // Listen the app at PORT
 app.listen(PORT, () => {
