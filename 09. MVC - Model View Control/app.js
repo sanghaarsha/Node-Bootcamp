820044fad2ceb0c10025afb122ae7eb961f38217
sanghaarsha <sanghaarsha@gmail.com>
2021-07-28 10:12:44 +0545

    404 setup

--- 09. MVC - Model View Control/app.js
+++ 09. MVC - Model View Control/app.js
@@ -15,4 +15,8 @@ app.use(express.static(__dirname + "/public"));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// 404
+app.get("*", (req, res) => {
+  res.status(404).sendFile(__dirname + "/public/404.html");
+});
 module.exports = app;
