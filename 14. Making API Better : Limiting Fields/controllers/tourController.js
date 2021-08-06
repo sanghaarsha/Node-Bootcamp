7feffa187525eb82351c44f2f9213117d3eb87bd
sanghaarsha <sanghaarsha@gmail.com>
2021-08-06 22:39:54 +0545

    init

---
+++ 14. Making API Better : Limiting Fields/controllers/tourController.js
@@ -0,0 +1,116 @@
+const Tour = require("./../models/tourModel");
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // Filtering :
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // Advanced filtering : (<,<=,>,>=)
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = JSON.parse(queryStr);
+
+    // Querying
+    let query = Tour.find(queryStr);
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // Awaiting document
+    const tours = await query;
+
+    // RESPONSE
+    res.status(200).json({
+      status: "success",
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.getTourById = async (req, res) => {
+  const reqId = req.params.id;
+
+  try {
+    const tour = await Tour.findById(reqId);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.postNewTour = async (req, res) => {
+  const data = req.body;
+  try {
+    const newTour = await Tour.create(data);
+
+    res.status(201).json({
+      status: "success",
+      data,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: "Invalid Data !",
+    });
+  }
+};
+
+exports.patchTour = async (req, res) => {
+  try {
+    const newTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: "Invalid Data !",
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: "Invalid Data !",
+    });
+  }
+};
