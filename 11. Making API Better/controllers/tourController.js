0001a7dbf3299cc70db9a5a7cb597cd06d356ad1
sanghaarsha <sanghaarsha@gmail.com>
2021-08-06 21:07:22 +0545

    init

---
+++ 11. Making API Better/controllers/tourController.js
@@ -0,0 +1,96 @@
+const Tour = require("./../models/tourModel");
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.getTourById = async (req, res) => {
+  const reqId = req.params.id;
+
+  try {
+    // findingById is just a mongoose helper function for
+    // mongoDB query using filter object like this :
+    // Tour.findOne({_id: reqId})
+    const tour = await Tour.findById(reqId);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.postNewTour = async (req, res) => {
+  const data = req.body;
+  try {
+    const newTour = await Tour.create(data);
+
+    res.status(201).json({
+      status: "success",
+      data,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: "Invalid Data !",
+    });
+  }
+};
+
+exports.patchTour = async (req, res) => {
+  try {
+    const newTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // returns new document (updated one)
+      runValidators: true, // validate update operation against the Tour Model's schema
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: "Invalid Data !",
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: "Invalid Data !",
+    });
+  }
+};
---
+++ 11. Making API Better/models/tourModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+// Creating Schema:
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name!"],
+    unique: true,
+    trim: true, // removes whitespaces from start and end of input
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration!"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size!"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a diificulty!"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price!"],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, "A tour must have a summary!"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image!"],
+  },
+  images: [String], // array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+});
+
+// Creating Model from Schema:
+const Tour = mongoose.model("Tour", tourSchema);
+
+// exporting Tour model
+module.exports = Tour;
