a073c44f68db9e140dde6d6997bc76bad178be9f
sanghaarsha <sanghaarsha@gmail.com>
2021-08-06 17:27:29 +0545

    reading documents

--- 09. MVC - Model View Control/controllers/tourController.js
+++ 09. MVC - Model View Control/controllers/tourController.js
@@ -1,25 +1,50 @@
 const fs = require("fs");
 const Tour = require("./../models/tourModel");
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    // getting tours from database
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
+      message: error,
+    });
+  }
 };
 
-exports.getTourById = (req, res) => {
-  const reqId = req.params.id * 1;
-  res.status(200).json({});
+exports.getTourById = async (req, res) => {
+  const reqId = req.params.id;
+
+  try {
+    // findingById is just a mongoose helper function for
+    // mongoDB query using filter object like this :
+    // Tour.findOne({_id: reqId})
+    const tour = await Tour.findById(reqId);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
+      message: error,
+    });
+  }
 };
 
 exports.postNewTour = async (req, res) => {
   const data = req.body;
-
-  // const newTour = new Tour(data);
-  // newTour.save(.sth..).then(sth...).catch(sth...) (because it returns promise)
-
-  // shorter way: (this also returns promise) so using async-await,
-  // and async-await needs try-catch to handle error
   try {
     const newTour = await Tour.create(data);
 
