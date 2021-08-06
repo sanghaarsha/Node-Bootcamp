23fcb9b0a830bfb9d955e57d1a63bcef8ef5060c
sanghaarsha <sanghaarsha@gmail.com>
2021-08-06 18:39:11 +0545

    modelling tours

--- 10. Modelling Tours/models/tourModel.js
+++ 10. Modelling Tours/models/tourModel.js
@@ -3,19 +3,55 @@ const mongoose = require("mongoose");
 // Creating Schema:
 const tourSchema = new mongoose.Schema({
   name: {
-    // schema type options
     type: String,
-    required: [true, "A tour must have a name!"], // validator
+    required: [true, "A tour must have a name!"],
     unique: true,
+    trim: true, // removes whitespaces from start and end of input
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration!"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size!"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a diificulty!"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
     type: Number,
     default: 0,
   },
   price: {
     type: Number,
-    required: [true, "A tour must have a price"],
+    required: [true, "A tour must have a price!"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, "A tour must have a summary!"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image!"],
+  },
+  images: [String], // array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // Creating Model from Schema:
