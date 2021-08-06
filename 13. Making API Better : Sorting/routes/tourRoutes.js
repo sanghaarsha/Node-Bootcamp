2615524932be27e352c5a6d7577c0cb4d31ef6b2
sanghaarsha <sanghaarsha@gmail.com>
2021-08-06 22:16:04 +0545

    init

---
+++ 13. Making API Better : Sorting/routes/tourRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const tourController = require("../controllers/tourController");
+
+// router setup
+const router = express.Router();
+
+// routes
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.postNewTour);
+
+router
+  .route("/:id")
+  .get(tourController.getTourById)
+  .patch(tourController.patchTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
