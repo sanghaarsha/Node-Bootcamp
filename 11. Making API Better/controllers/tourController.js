76f34cdf707870242be5b657a8b4d9adbdff84a3
sanghaarsha <sanghaarsha@gmail.com>
2021-08-06 21:38:13 +0545

    filtering by query

--- 11. Making API Better/controllers/tourController.js
+++ 11. Making API Better/controllers/tourController.js
@@ -2,7 +2,43 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Query Parameters
+    // copying object by value not reference (shallow copy), to make original object safe
+    const queryObj = { ...req.query };
+
+    // excluding special variable from query params
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // MongoDB's query method:
+    // const tours = await Tour.find({
+    //   duration:5,
+    //   difficulty:'easy'
+    // });
+
+    // Querying using mongoose:
+    // const tours = await Tour.find()
+    // .where("duration")
+    // .equals(5)
+    // .where("difficulty")
+    // .equals("easy");
+
+    // find tours by query parameters
+    // Tour.find(...) returns a query which when awaited
+    // returns a document that satisfies the query object
+    // const tours = await Tour.find(queryObj);
+
+    // for pagination we cannot await for result as
+    // further query needs to be done to original query
+    // so instead of awaiting like above, we will store
+    // query in a variable, then perform more queries to it
+    // then finally await for document, like this :
+
+    // this is called query chaining
+    const query = Tour.find(queryObj);
+    // query.sort();
+    // query.page();
+    const tours = await query;
 
     res.status(200).json({
       status: "success",
