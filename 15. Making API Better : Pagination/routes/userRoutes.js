6099cf3d9a1a5dc54acaeed3aacfb4311e432c4a
sanghaarsha <sanghaarsha@gmail.com>
2021-08-07 11:45:27 +0545

    init

---
+++ 15. Making API Better : Pagination/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const userController = require("../controllers/userController");
+
+const router = express.Router();
+
+// param middleware
+router.param("id", userController.checkId);
+
+// routes
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
